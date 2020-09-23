94d1c875d3dcbc10b3608e3d2609c6c965f38fed
People-11 <41558215+People-11@users.noreply.github.com>
2020-09-23 12:53:27 +0800

    Add SCUTClient

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add SCUTClient
+git clone https://github.com/scutclient/scutclient package/scutclient
